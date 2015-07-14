180d8d8fc287dc27a0b3088302aa87868cdbdd97
rgmchetumal <rgmchetumal@gmail.com>
2015-07-14 06:16:09 -0500

    Versión para despliegue de base de datos con Postgres

--- models/models.js
+++ models/models.js
@@ -1,11 +1,30 @@
 var path = require ('path');
 
+// Postgres DATABASE_URL = postgress://user:paswd@host:port/database
+// SQlite DATABASE_URL = sqlit://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name    = (url[6] || null);
+var user       = (url[2] || null);
+var pwd        = (url[3] || null);
+var protocol   = (url[1] || null);
+var dialect    = (url[1] || null);
+var port       = (url[5] || null);
+var host       = (url[4] || null);
+var storage    = process.env.DATABASE_STORAGE;
+
 // Cargar modelo ORM
 var Sequelize = require('sequelize');
 
-// Usar BBDD SQLite
-var sequelize = new Sequelize ( null, null, null,
-	                              { dialect: "sqlite", storage: "quiz.sqlite"}
+
+// Usar BBDD SQLite o Postgres
+var sequelize = new Sequelize ( DB_name, user, pwd,
+	                              {    dialect: protocol,
+                                        protocol: protocol,
+                                        port: port,
+                                        host: host,
+                                        storage: storage, // Sólo SQlite (.env)
+                                        omitNull: true // Sólo Postgres
+                                   }
 	                           );
 
 // Importar la definición de la tabla Quiz en quiz.js
