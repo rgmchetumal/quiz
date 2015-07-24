5c4faa752bc4365232ce09ef5560dffbc3f50363
rgmchetumal <rgmchetumal@gmail.com>
2015-07-24 07:15:20 -0500

    Versión que satisface petición ejercicio P2P módulo 8

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -102,7 +102,7 @@ exports.answer = function (req, res) {
 // GET /quizes/new
 exports.new = function(req, res){
   var quiz = models.Quiz.build(  // crea objeto quiz
-    {pregunta: "Pregunta", respuesta: "Respuesta"}
+    {pregunta: "Pregunta", respuesta: "Respuesta", tema: 'Otro'}
   );
 
   res.render('quizes/new', {quiz: quiz, errors: []});
@@ -117,11 +117,16 @@ exports.create = function(req, res) {
   .then(
     function(err) {
       if (err) {
+
+        
         res.render('quizes/new', {quiz: quiz, errors: err.errors});
+
       } else {
+
         quiz  // save: guarda en DB campos y pregunta y respuesta de quiz
-        .save({fields: ["pregunta", "respuesta"]})
+        .save({fields: ["pregunta", "respuesta", "tema"]})
         .then( function(){ res.redirect('/quizes')})
+        
       }   // res.redirect: Redirección HTTP a lista de preguntas
     }
   );
@@ -140,18 +145,22 @@ exports.edit = function (req, res) {
 exports.update = function (req, res) {
      req.quiz.pregunta  = req.body.quiz.pregunta;
      req.quiz.respuesta = req.body.quiz.respuesta;
+     req.quiz.tema = req.body.quiz.tema;
 
      req.quiz
      .validate()
      .then (
             function (err) {
                   if (err) {
+
                       res.render('quizes/edit', { quiz: req.quiz, errors: err.errors });
+
                   } else {
                       
                       req.quiz // save: guarda campos pregunta y respuesta en base de datos
-                      .save( {fields: ["pregunta", "respuesta"]} )
+                      .save( {fields: ["pregunta", "respuesta", "tema"]} )
                       .then ( function () { res.redirect ('/quizes'); });
+
                   } // Redirección HTTP a lista de preguntas (URL relativo)
             }
       );
--- models/models.js
+++ models/models.js
@@ -63,12 +63,14 @@ sequelize.sync().then( function() {
                // La tabla se inicializa sólo si está vacía
                Quiz.create({ 
                     pregunta: '¿Capital de Italia?', 
-                    respuesta: 'Roma'
+                    respuesta: 'Roma',
+                    tema: 'Otro'
                });
                
                Quiz.create({ 
                     pregunta: '¿Capital de Portugal?', 
-                    respuesta: 'Lisboa'
+                    respuesta: 'Lisboa',
+                    tema: 'Otro'
                }).then( function() {console.log ('Base de datos inicializada')});
 
           };
